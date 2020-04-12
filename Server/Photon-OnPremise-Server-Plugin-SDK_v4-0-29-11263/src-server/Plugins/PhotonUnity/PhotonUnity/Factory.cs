b1529ec2f67bc784fb3e04e0be528d96ef4a331e
khanhdrag9 <tkhanh783@gmail.com>
2020-04-12 13:21:41 +0700

    Update Lan server config

---
+++ Server/Photon-OnPremise-Server-Plugin-SDK_v4-0-29-11263/src-server/Plugins/PhotonUnity/PhotonUnity/Factory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Photon.Hive.Plugin;
+
+namespace PhotonUnity
+{
+    class Factory : IPluginFactory
+    {
+        public IGamePlugin Create(IPluginHost gameHost, string pluginName, Dictionary<string, string> config, out string errorMsg)
+        {
+            PhotonUnity output = new PhotonUnity();
+            bool r = output.SetupInstance(gameHost, config, out errorMsg);
+            return r ? output : null;
+        }
+    }
+}
---
+++ Server/Photon-OnPremise-Server-Plugin-SDK_v4-0-29-11263/src-server/Plugins/PhotonUnity/PhotonUnity/PhotonUnity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Photon.Hive.Plugin;
+
+namespace PhotonUnity
+{
+    public class PhotonUnity : PluginBase
+    {
+        public override string Name => "PhotonUnity";
+        public override void OnCreateGame(ICreateGameCallInfo info)
+        {
+            info.Continue();
+        }
+    }
+}
